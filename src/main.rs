f4690a7a767175d29382d68470b8d44907233bd1
James Rhodes <james.rhodes@hotmail.com.au>
2024-05-24 22:47:45 +1000

    Fix: Reduce default size and samples

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,12 @@ use std::f32::consts::PI;
 use macroquad::prelude::*;
 use mqanim::Animation;
 
-const WINDOW_WIDTH: f32 = 1280.0;
-const WINDOW_HEIGHT: f32 = 720.0;
+const WINDOW_WIDTH: f32 = 640.0;
+const WINDOW_HEIGHT: f32 = 360.0;
 fn window_conf() -> Conf {
     Conf {
         window_title: "Template".to_owned(),
-        sample_count: 16,
+        sample_count: 4,
         window_width: WINDOW_WIDTH as i32,
         window_height: WINDOW_HEIGHT as i32,
         ..Default::default()
